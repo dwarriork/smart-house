abfef61580404b21683b968cf84102c2bb2718e4
Yanay <dwarriork@gmail.com>
2020-03-10 01:07:04 +0200

    Updates

---
+++ precache-manifest.70bd44c6174813b53525c7b7a9a2fcd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d3e1664631c2565a4de9e0ec218277",
+    "url": "/smart-house/index.html"
+  },
+  {
+    "revision": "187f08e8b2ae7b22f209",
+    "url": "/smart-house/static/css/main.2271b8a0.chunk.css"
+  },
+  {
+    "revision": "7eed7bf14ff787aacefe",
+    "url": "/smart-house/static/js/2.624549ca.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/smart-house/static/js/2.624549ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "187f08e8b2ae7b22f209",
+    "url": "/smart-house/static/js/main.558518a7.chunk.js"
+  },
+  {
+    "revision": "68c4136c18106bf7065d",
+    "url": "/smart-house/static/js/runtime-main.6fd0324a.js"
+  }
+]);
